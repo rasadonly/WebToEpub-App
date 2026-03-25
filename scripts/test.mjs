fe915d6dacd99fb4097782582346ab0dac73d7f7
prasadghanwatprof-lang <prasadghanwatprof@gmail.com>
2026-03-25 21:29:08 +0530

    Final cleanup of diagnostic files

--- scripts/test.mjs
+++
@@ -1,5 +0,0 @@
-import { parseHTML } from 'linkedom';
-console.log("Linkedom loaded");
-const { window } = parseHTML('<html></html>');
-console.log("DOM parsed");
-console.log("Window exists:", !!window);
